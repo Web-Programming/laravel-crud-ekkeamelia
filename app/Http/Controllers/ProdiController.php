83cbedae0a45192b844678bcfa8f27e620b285a2
ekkeamelia <ameliaekke53@gmail.com>
2022-05-13 11:09:36 +0700

    file upload

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -24,16 +24,25 @@ class ProdiController extends Controller
         return view("prodi.create");
     }
 
+
     public function store(Request $request)
     {
         $validateData = $request->validate([
             'nama' => 'required|min:5|max:20',
+            'foto' => 'required|file|image|max:5000'
         ]);
-       
-        $prodi = new Prodi(); //buat object prodi
-        $prodi->nama = $validateData['nama']; //simpan nilai inout ($validateData['nama]) ke dalam property nama prodi ($prodi->nama)
+        //mengambil file extension
+        $ext = $request->foto->getClientOriginalExtension();
+        //menentukan nama file 
+        $nama_file = "foto-" . time() . "." . $ext;
+        $path = $request->foto->storeAs("public", $nama_file);
+
+
+        $prodi = new Prodi(); //buat object prodi 
+        $prodi->nama = $validateData['nama']; //simpan nilai input ($validateData['nama]) ke dalam property nama prodi ($prodi->nama)
         $prodi->institusi_id = 0;
         $prodi->fakultas_id = 1;
+        $foto->foto= $nama_file;
         $prodi->save(); //simpan ke dalam tabel prodis
 
         //return "Data prodi $prodi->nama berhasil disimpan ke database"; // tampilkan pesan berhasil
